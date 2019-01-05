2164684cd4b5ef890a764caf257cc2e47331c58d
NObao <nobao.code@gmail.com>
2019-01-06 01:00:49 +0300

    После урока 013 Массивы и псевдомассивы

--- Lessons/script.js
+++ Lessons/script.js
@@ -20,3 +20,42 @@ for (let key in options) {
 }
 
 console.log(Object.keys(options).length);
+
+// let arr = ['first', 2, 3, 'four', 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// arr.forEach(function(item, i, mass) {
+//     console.log(i + ': ' + item + ' (массив: ' + mass + ')');
+// });
+
+
+// console.log(arr);
+
+// let mass = [1, 3, 4, 6, 7];
+
+// for (let key of mass) {
+//     console.log(key);
+// }
+
+// let ans = prompt('', ''),
+//     arr;
+
+// arr = ans.split(', ');
+// console.log(arr);
+
+// let arr = ['food', 'storm', 'goal', 'transform'],
+//     i = arr.join(', ');
+
+// console.log(i);
+
+let arr = [1, 15, 4, 26],
+    i = arr.sort(compareNum);
+
+function compareNum(a,b) {
+    return a-b;
+}
+
+console.log(arr);
