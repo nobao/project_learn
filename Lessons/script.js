35f67ced5e1ae395eb88e006f0dbd72f86d81ccb
NObao <nobao.code@gmail.com>
2019-01-06 21:29:42 +0300

    После урока 014 ООП

--- Lessons/script.js
+++ Lessons/script.js
@@ -59,3 +59,19 @@ function compareNum(a,b) {
 }
 
 console.log(arr);
+
+let soldier = {
+    health: 400,
+    armor: 100
+};
+
+let john = {
+    health: 100
+};
+
+john.__proto__  = soldier;
+
+console.log(john);
+console.log(john.armor);
+
+
