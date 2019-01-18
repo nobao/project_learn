eb6cc4947c28cbbc82da0715c03c28bdaf30a2f0
NObao <nobao.code@gmail.com>
2019-01-19 00:07:39 +0300

    После 023 Контекст вызова (this)

--- Lessons/script.js
+++ Lessons/script.js
@@ -20,22 +20,83 @@
 // ivan.exit();
 
 //ES6
-class User {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-        this.human = true;
+// class User {
+//     constructor(name, id) {
+//         this.name = name;
+//         this.id = id;
+//         this.human = true;
+//     }
+//     hello() {
+//         console.log(`Hello! ${this.name}`);
+//     }
+//     exit() {
+//         console.log(`Пользователь ${this.name} ушел`);
+//     }
+// }
+// let ivan = new User('Ivan', 25),
+//     alex = new User('Alex', 20);
+// console.log(ivan);
+// console.log(alex);
+// ivan.hello();
+// alex.hello();
+
+// 'use strict';
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+// showThis(4, 5);
+// showThis(5, 5);
+
+// let obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
+// obj.sum();
+
+// let user = {
+//     name: "John"
+// };
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// console.log(sayName.call(user, 'Smith'));
+// console.log(sayName.apply(user, ['Snow']));
+
+// function count(number) {
+//     return this * number;
+// }
+
+// let double = count.bind(2);
+// console.log(double(3));
+// console.log(double(10));
+
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    console.log(this);
+    this.style.backgroundColor = 'red';
+    function showThis() {
+        console.log(this);
     }
-    hello() {
-        console.log(`Hello! ${this.name}`);
-    }
-    exit() {
-        console.log(`Пользователь ${this.name} ушел`);
-    }
-}
-let ivan = new User('Ivan', 25),
-    alex = new User('Alex', 20);
-console.log(ivan);
-console.log(alex);
-ivan.hello();
-alex.hello();
+    showthis();
+});
+
+// 1) Просто вызов функции - window/undefined
+// 2) Метод объекта - this = объект
+// 3) Конструктор (new) - this = новый созданный объект
+// 4) указание конкретного контекста - call, apply, bind
