adcacc9d700b520bb11ec9c1fe4dd9f6947fe419
NObao <nobao.code@gmail.com>
2019-01-20 21:30:45 +0300

    После 028 ES6. Классы

--- Lessons/script.js
+++ Lessons/script.js
@@ -159,3 +159,17 @@ function calcOrDouble(number, basis = 2) {
 }
 calcOrDouble(3,5);
 calcOrDouble(6);
+
+class Rectangle {
+    constructor(height, width = 20) {
+        this.height = height;
+        this.width = width;
+    }
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+const square = new Rectangle(10);
+
+console.log(square.calcArea());
