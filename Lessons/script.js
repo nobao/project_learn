ab65b31201c4acf3dbfe1bad4bb48811066646f4
NObao <nobao.code@gmail.com>
2019-01-20 13:47:44 +0300

    После 025 ES6. let и const

--- Lessons/script.js
+++ Lessons/script.js
@@ -106,3 +106,22 @@ let name = 'Ivan',
     mail = 'example@mail.ru';
 
 document.write(`Пользователю ${name} ${age} лет. Его почтовый адрес: ${mail}`);
+
+function makeArray() {
+    var items = [];
+
+    for (let i = 0; i < 10; i++) {
+        var item = function() {
+            console.log(i);
+        };
+        items.push(item);
+    }
+
+    return items;
+}
+
+var arr = makeArray();
+
+arr[1]();
+arr[3]();
+arr[7]();
