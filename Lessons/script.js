f21281a110db0bcd7ca8db75c8ae842384c0b0ff
NObao <nobao.code@gmail.com>
2019-01-20 21:05:03 +0300

    После 026 ES6. Стрелочные функции

--- Lessons/script.js
+++ Lessons/script.js
@@ -125,3 +125,30 @@ var arr = makeArray();
 arr[1]();
 arr[3]();
 arr[7]();
+
+let fun = () => {
+    console.log(this);
+};
+
+fun();
+
+let obj = {
+    number: 5,
+    sayNumber: function() {
+        let say = () => {
+            console.log(this);
+        };
+        say();
+    }
+};
+
+obj.sayNumber();
+
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    let show = () => {
+        console.log(this);
+    };
+    show();
+});
