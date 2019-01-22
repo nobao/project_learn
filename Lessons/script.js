62b79b3ac5e82b80f83e27d29197ec818fcf148a
NObao <nobao.code@gmail.com>
2019-01-23 00:16:20 +0300

    После 031 AJAX

--- Lessons/script.js
+++ Lessons/script.js
@@ -1,11 +1,38 @@
-let options = {
-    width: 1366,
-    height: 768,
-    background: 'red',
-    font: {
-        size: '16px',
-        color: '#fff'
-    }
-};
+// let options = {
+//     width: 1366,
+//     height: 768,
+//     background: 'red',
+//     font: {
+//         size: '16px',
+//         color: '#fff'
+//     }
+// };
 
-console.log(JSON.parse(JSON.stringify(options)));
+// console.log(JSON.parse(JSON.stringify(options)));
+
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    let request = new XMLHttpRequest();
+
+    // request.open(method, url, async, login, pass)
+    request.open('GET', './current.json');
+    request.setRequestHeader('Content-type', 'aapplication/json; charset=utf-8');
+    request.send();
+
+    //status
+    //statusText
+    //responseText / response
+    //readyState
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState === 4 && request.status == 200) {
+            let data = JSON.parse(request.response);
+
+            inputUsd.value = inputRub.value / data.usd;
+        } else {
+            inputUsd.value = "Что-то пошло не так!";
+        }
+    });
+});
