20871ceb2a34beba935e8bfb8cd82946ce8278dd
NObao <nobao.code@gmail.com>
2019-01-27 23:32:06 +0300

    После 032 ES6. Promise

---
+++ Lessons/promise.js
@@ -0,0 +1,76 @@
+// без Promise
+// let drink = 0;
+
+// function shoot(arrow, headshot, fail) {
+//     console.log('Вы сделали выстрел...');
+
+//     setTimeout(function () {
+//        Math.random() > 0.5 ? headshot({}) : fail('Вы промахнулись'); 
+//     }, 3000);
+// }
+
+// function win() {
+//     console.log('Вы победили!');
+//     (drink == 1) ? buyBeer() : giveMoney();
+// }
+
+// function buyBeer() {
+//     console.log('Вам купили пиво');
+// }
+
+// function giveMoney() {
+//     console.log('Вам заплатили');
+// }
+
+// function loose() {
+//     console.log('Вы проиграли!');
+// }
+
+// shoot({},
+//         function(mark) {
+//             console.log('Вы попали в цель');
+//             win(mark, buyBeer, giveMoney);
+//         },
+//         function(miss) {
+//             console.error(miss);
+//             loose();
+//         }
+//     );
+
+// с применением Promise
+let drink = 0;
+
+function shoot(arrow) {
+    console.log('Вы сделали выстрел...');
+
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(function () {
+            Math.random() > 0.5 ? resolve({}) : reject('Вы промахнулись'); 
+         }, 3000);
+    });
+
+    return promise;
+
+}
+
+function win() {
+    console.log('Вы победили!');
+    (drink == 1) ? buyBeer() : giveMoney();
+}
+
+function buyBeer() {
+    console.log('Вам купили пиво');
+}
+
+function giveMoney() {
+    console.log('Вам заплатили');
+}
+
+function loose() {
+    console.log('Вы проиграли!');
+}
+
+shoot({})
+        .then(mark => console.log('Вы попали в цель!'))
+        .then(win)
+        .catch(loose)
