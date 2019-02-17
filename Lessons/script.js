669f95b596856a0dae45117fb855f86faae8d4de
NObao <nobao.code@gmail.com>
2019-02-17 23:32:01 +0300

    После 036 Инкапсуляция. Геттеры и сеттеры

--- Lessons/script.js
+++ Lessons/script.js
@@ -1,18 +1,31 @@
-$(document).ready(function() {
-   $('.list-item:first').hover(function() {
-       $(this).toggleClass('active');
-   });
-   
-   $('.list-item:eq(2)').on('click', function() {
-       $('.image:even').fadeToggle('slow');
-   });
+function User (name, age) {
+    this.name = name;
+    let userAge = age;
 
-   $('.list-item:eq(4)').on('click', function() {
-      $('.image:odd').animate(
-          {
-              opacity: 'toggle',
-              height: 'toggle'
-          }, 3000
-        ); 
-   });
-});
+    this.getAge = function() {
+        return userAge;
+    };
+    this.setAge = function(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            userAge = age;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    };
+    this.say = function() {
+        console.log(`Имя пользователя ${this.name}, возраст: ${this.userAge}`);
+    }
+}
+
+let user = new User ('Ivan', 25);
+console.log(user.name);
+console.log(user.userAge);
+user.say();
+console.log(user.getAge());
+user.setAge('30');
+console.log(user.getAge());
+
+
+// user.name = 'Alex';
+// user.age = 30;
+// user.say();
