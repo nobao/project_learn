53f615aebd4b71fe7c219ef67004bf6923b5ef36
NObao <nobao.code@gmail.com>
2019-02-19 23:25:59 +0300

    После 037 Модули

--- Lessons/script.js
+++ Lessons/script.js
@@ -1,31 +1,63 @@
-function User (name, age) {
-    this.name = name;
-    let userAge = age;
 
-    this.getAge = function() {
-        return userAge;
-    };
-    this.setAge = function(age) {
-        if (typeof age === 'number' && age > 0 && age < 110) {
-            userAge = age;
-        } else {
-            console.log('Недопустимое значение!');
-        }
-    };
-    this.say = function() {
-        console.log(`Имя пользователя ${this.name}, возраст: ${this.userAge}`);
-    }
-}
+// let app = {
+//     data: 45
+// };
+// console.log(app);
 
-let user = new User ('Ivan', 25);
-console.log(user.name);
-console.log(user.userAge);
-user.say();
-console.log(user.getAge());
-user.setAge('30');
-console.log(user.getAge());
+// function User (name, age) {
+//     this.name = name;
+//     let userAge = age;
+
+//     this.getAge = function() {
+//         return userAge;
+//     };
+//     this.setAge = function(age) {
+//         if (typeof age === 'number' && age > 0 && age < 110) {
+//             userAge = age;
+//         } else {
+//             console.log('Недопустимое значение!');
+//         }
+//     };
+//     this.say = function() {
+//         console.log(`Имя пользователя ${this.name}, возраст: ${this.userAge}`);
+//     }
+// }
+
+// let user = new User ('Ivan', 25);
+// console.log(user.name);
+// console.log(user.userAge);
+// user.say();
+// console.log(user.getAge());
+// user.setAge('30');
+// console.log(user.getAge());
 
 
 // user.name = 'Alex';
 // user.age = 30;
 // user.say();
+
+// let number = 1;
+
+// (function() {
+//     let number = 2;
+//     console.log(number);
+
+//     return console.log(number + 3);
+// }());
+
+// console.log(number);
+
+let user = (function() {
+    let private = function() {
+        console.log("I'm private");
+    };
+    let sayHello = function() {
+        console.log('Hello!');
+    }
+    return {
+        sayHello: sayHello    
+    }
+}());
+
+console.log(user);
+console.log(user.sayHello());
